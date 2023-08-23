f7939cc7efcba8b82a0a578979cbcbf10adef92f
Emino08 <koromaemmanuel66@gmail.com>
2023-08-23 00:13:26 +0000

    AirBnB_clone

---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Defines city
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """defines city to look for"""
+    state_id = ""
+    name = ""
